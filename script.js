a89e4066e67a5670cdf24584dfe3337d6000a607
sarthak288 <sarthakbhar55@gmail.com>
2025-02-06 12:07:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Scroll effect (you can add more functionality as needed)
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
